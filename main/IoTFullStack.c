03e4028d4344f8b316e40240ba5c4f1d80805d6d
alitaroosheh <ali.taroosheh@gmail.com>
2024-07-16 03:46:31 +0200

    NVSHelper imported to the project

--- main/IoTFullStack.c
+++ main/IoTFullStack.c
@@ -11,55 +11,54 @@
 
 #include "IoTFullStack.h"
 
-#define TAG "IoTFullStack"
+#define TAG						"IoTFullStack"
+#define configPartition       	"config_nvs"
+#define configNamespace			"config"
 
 /// @brief app_main: starting point of the application.
 /// @param
 void app_main(void)
 {
-	esp_err_t ret = nvs_flash_init();
-	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
-	{
-		ESP_ERROR_CHECK(nvs_flash_erase());
-		ret = nvs_flash_init();
-	}
-
-	ESP_ERROR_CHECK(ret);
 
 	uint32_t loopInterval = 5000;
 
-	esp_err_t err = nvs_flash_init();
-	if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND)
-	{
-		ESP_ERROR_CHECK(nvs_flash_erase());
-		err = nvs_flash_init();
-	}
-	ESP_ERROR_CHECK(err);
+	nvsInit(configPartition, false);
 
 	wifiInit();
-	wifi_config_t wifiConfigSTA = {0};
-
-    size_t len = sizeof(wifiConfigSTA.sta.ssid);
-
-	if (nvsLoad("wifi", "ssid", (char *)wifiConfigSTA.sta.ssid, &len) != ESP_OK)
-	{
-		ESP_LOGI(TAG, "wifi STA SSID info doesn't exist...");
-        nvsSave("wifi", "ssid", "sta-wifi", strlen("sta-wifi"));
-        nvsLoad("wifi", "ssid", (char *)wifiConfigSTA.sta.ssid, &len);
-	}
-    len = sizeof(wifiConfigSTA.sta.password);
-    if (nvsLoad("wifi", "password", (char *)wifiConfigSTA.sta.password, &len) != ESP_OK)
-	{
-		ESP_LOGI(TAG, "wifi STA password info doesn't exist...");
-        nvsSave("wifi", "password", "password", strlen("password"));
-        nvsLoad("wifi", "password", (char *)wifiConfigSTA.sta.password, &len);
-	}
 
 	
-	ESP_LOGI(TAG, "wifi STA SSID: \"%s\" password: \"%s\"", (char *)wifiConfigSTA.sta.ssid, (char *)wifiConfigSTA.sta.password);
+	ESP_LOGI(TAG, "IoT Full Stack application is running...");
+	//vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+
+	wifi_config_t sta = {0};
+    size_t len = sizeof(sta.sta.ssid);
+	if (nvsLoad(configPartition, configNamespace, "ssid", (char *)sta.sta.ssid, &len) != ESP_OK)
+	{
+		nvsErasePartition(configPartition);
+		ESP_LOGI(TAG, "wifi STA SSID info doesn't exist...");
+        nvsSave(configPartition, configNamespace, "ssid", "sta-wifi", strlen("sta-wifi"));
+        nvsLoad(configPartition, configNamespace, "ssid", (char *)sta.sta.ssid, &len);
+	}
+
+    len = sizeof(sta.sta.password);
+
+    if (nvsLoad(configPartition, configNamespace,  "password", (char *)sta.sta.password, &len) != ESP_OK)
+	{
+		ESP_LOGI(TAG, "wifi STA password info doesn't exist...");
+        nvsSave(configPartition, configNamespace,  "password", "password", strlen("password"));
+        nvsLoad(configPartition, configNamespace,  "password", (char *)sta.sta.password, &len);
+	}
+
+	// wifi_config_t sta = {0};
+	// strcpy((char *)sta.sta.ssid, "sta-wifi");
+	// strcpy((char *)sta.sta.password, "password");
+
+	
+	ESP_LOGI(TAG, "wifi STA SSID: \"%s\" password: \"%s\"", (char *)sta.sta.ssid, (char *)sta.sta.password);
 
 	ESP_LOGI(TAG, "Start STA Mode");
-	ret = wifiStartSTA(wifiConfigSTA, 5 * 1000);
+	esp_err_t ret = wifiStartSTA(sta, 5 * 1000);
 	ESP_LOGI(TAG, "WiFiStartSTA: %d", ret);
 
 	xTaskCreate(&iaqTask, "IAQTask", configMINIMAL_STACK_SIZE * 2, (void *)&loopInterval, 5, NULL);
