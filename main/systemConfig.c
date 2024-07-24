aa078361df8353e5826173bd0bb6552aa08d9d90
alitaroosheh <ali.taroosheh@gmail.com>
2024-07-24 13:04:15 +0200

    readMqttConfig & readWiFiConfig functions added

--- main/systemConfig.c
+++ main/systemConfig.c
@@ -1,4 +1,12 @@
-
+/**
+ * @file systemConfig.c
+ * @author Ali Taroosheh (ali.taroosheh@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-07-24
+ * 
+ * 
+ */
 
 #include "IoTFullStack.h"
 
@@ -6,49 +14,21 @@
 
 #define TAG						"systemConfig"
 
-typedef struct 
+
+static systemConfig confing;
+
+
+static esp_err_t readWiFiConfig(uint8_t *json, wifiConfig *wifi)
 {
-    uint8_t topic[128];
-    uint8_t message[128];
-    uint8_t qos;
-    bool retain;
-}lastwillConfig;
 
-typedef struct 
-{
-    uint8_t clientCert[4096];
-    uint8_t serverCert[4096];
-}certConfig;
+    cJSON *root = cJSON_Parse((char *)json);
+    jsonGetString(root, "ssid", wifi->ssid);
+    jsonGetString(root, "password", wifi->password);
 
+    cJSON_Delete(root);
+    return ESP_OK;
 
-
-typedef struct 
-{
-    uint8_t clientId[32];
-    uint8_t url[256];
-    esp_mqtt_protocol_ver_t version;
-    uint8_t userName[128];
-    uint8_t password[128];
-    lastwillConfig lastwill;
-    certConfig cert;
-}mqttConfig;
-
-typedef struct 
-{
-    uint8_t ssid[128];
-    uint8_t password[128];
-}wifiConfig;
-
-typedef struct systemConfig
-{
-    uint8_t version[6];
-    wifiConfig wifi;
-    mqttConfig mqtt;
-}systemConfig;
-
-
-
-static systemConfig sysConfing;
+}
 
 
 esp_err_t systemConfigLoadWiFi()
@@ -61,9 +41,24 @@ esp_err_t systemConfigLoadWiFi()
         return ESP_ERR_NOT_FOUND;
     }
 
+    return readWiFiConfig(json, &confing.wifi);
+}
+
+
+static esp_err_t readMqttConfig(uint8_t *json, mqttConfig *mqtt)
+{
     cJSON *root = cJSON_Parse((char *)json);
-    jsonGetString(root, "ssid", sysConfing.wifi.ssid);
-    jsonGetString(root, "password", sysConfing.wifi.password);
+    jsonGetString(root, "clientId", mqtt->clientId);
+    jsonGetString(root, "url", mqtt->url);
+    jsonGetInt(root, "version", &mqtt->version);
+    jsonGetString(root, "userName", mqtt->userName);
+    jsonGetString(root, "password", mqtt->password);
+
+    cJSON *lastwill = cJSON_GetObjectItem(root, "lastwill");
+    jsonGetString(lastwill, "topic", mqtt->lastwill.topic);
+    jsonGetString(lastwill, "message", mqtt->lastwill.message);
+    jsonGetInt(lastwill, "qos", &mqtt->lastwill.qos);
+    jsonGetInt(lastwill, "retain", &mqtt->lastwill.retain);
 
     cJSON_Delete(root);
     return ESP_OK;
@@ -72,6 +67,7 @@ esp_err_t systemConfigLoadWiFi()
 
 esp_err_t systemConfigLoadMqtt()
 {
+
     size_t volatile len;
     uint8_t json[10240] = {0};
     if (nvsLoad(configPartition, "mqtt", "mqtt", (char *)json, (size_t *)&len) != ESP_OK)
@@ -80,19 +76,6 @@ esp_err_t systemConfigLoadMqtt()
         return ESP_ERR_NOT_FOUND;
     }
 
-    cJSON *root = cJSON_Parse((char *)json);
-    jsonGetString(root, "clientId", sysConfing.mqtt.clientId);
-    jsonGetString(root, "url", sysConfing.mqtt.url);
-    jsonGetInt(root, "version", &sysConfing.mqtt.version);
-    jsonGetString(root, "userName", sysConfing.mqtt.userName);
-    jsonGetString(root, "password", sysConfing.mqtt.password);
+    return readMqttConfig(json, &confing.mqtt);
 
-    cJSON *lastwill = cJSON_GetObjectItem(root, "lastwill");
-    jsonGetString(lastwill, "topic", sysConfing.mqtt.lastwill.topic);
-    jsonGetString(lastwill, "message", sysConfing.mqtt.lastwill.message);
-    jsonGetInt(lastwill, "qos", &sysConfing.mqtt.lastwill.qos);
-    jsonGetInt(lastwill, "retain", &sysConfing.mqtt.lastwill.retain);
-
-    cJSON_Delete(root);
-    return ESP_OK;
 }
--- main/systemConfig.h
+++ main/systemConfig.h
@@ -1,3 +1,13 @@
+/**
+ * @file systemConfig.h
+ * @author Ali Taroosheh (ali.taroosheh@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-07-24
+ * 
+ * 
+ */
+
 
 #pragma once
 
@@ -17,7 +27,45 @@
 #include "mqtt_client.h"
 #include "cJSON.h"
 
+typedef struct 
+{
+    uint8_t topic[128];
+    uint8_t message[128];
+    uint8_t qos;
+    bool retain;
+}lastwillConfig;
 
+typedef struct 
+{
+    uint8_t clientCert[4096];
+    uint8_t serverCert[4096];
+}certConfig;
+
+
+
+typedef struct 
+{
+    uint8_t clientId[32];
+    uint8_t url[256];
+    esp_mqtt_protocol_ver_t version;
+    uint8_t userName[128];
+    uint8_t password[128];
+    lastwillConfig lastwill;
+    certConfig cert;
+}mqttConfig;
+
+typedef struct 
+{
+    uint8_t ssid[128];
+    uint8_t password[128];
+}wifiConfig;
+
+typedef struct systemConfig
+{
+    uint8_t version[6];
+    wifiConfig wifi;
+    mqttConfig mqtt;
+}systemConfig;
 
 
 esp_err_t                   systemConfigLoadWiFi                                      ();
